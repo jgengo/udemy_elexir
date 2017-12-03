045bf86f7235daca2e8d219644acd787762d4d71
Jordane GENGO <jgengo@e3r1p2.42.fr>
2017-12-03 19:16:19 +0100

    udemy :P

---
+++ lib/cards.ex
@@ -0,0 +1,40 @@
+defmodule Cards do
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+    
+    cards = for  suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+
+    List.flatten(cards)
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    { status, content } = File.read(filename)
+
+    case status do
+      :ok -> :erlang.binary_to_term(content)
+      :error -> "That's file doesn't exist"
+    end
+    
+  end
+
+end
